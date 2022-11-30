5080371bb8c42837401333dc3242643128dfd1c2
Maksym Voloshyn <maxvol200727@gmail.com>
2022-11-30 21:00:11 +0200

    ready burger menu

--- src/App.tsx
+++ src/App.tsx
@@ -23,11 +23,31 @@ import { Product } from "./components/Product"
 
 function App() {
   const [signup,setSignup]=useState(false)
-  const [login,setLogin]=useState(true)
+  const [login,setLogin]=useState(false)
+  const [burger,setBurger]=useState(true)
 
 useEffect(()=>{
   axios.get("/posts")
 },[])
+function a() {
+  if(document.querySelector(".one")?.classList.contains("one")){
+  document.querySelector(".one")?.classList.add("first")
+  document.querySelector(".two")?.classList.add("second")
+  document.querySelector(".three")?.classList.add("third")
+  document.querySelector(".one")?.classList.remove("one")
+  document.querySelector(".two")?.classList.remove("two")
+  document.querySelector(".three")?.classList.remove("three")
+  setBurger(true)
+}else{
+  document.querySelector(".first")?.classList.add("one")
+  document.querySelector(".second")?.classList.add("two")
+  document.querySelector(".third")?.classList.add("three")
+  document.querySelector(".first")?.classList.remove("first")
+  document.querySelector(".second")?.classList.remove("second")
+  document.querySelector(".third")?.classList.remove("third")
+  setBurger(false)
+}
+}
 const {products,addProduct} = useProducts()
 
   return (
@@ -40,6 +60,22 @@ const {products,addProduct} = useProducts()
       </form>
       <button className="bg-red-600 px-2 py-1 rounded hover:bg-red-700 w-16 mt-5" onClick={()=>{setSignup(false)}}>Close</button>
       </Modal>}
+
+      {burger&&<Modal title="Menu" setSignup={setSignup}>
+        <div className="flex flex-col text-center mt-5">
+          <div className="">CATALOG</div>
+          <div className="">FASHION</div>
+          <div className="">FAVOURITE</div>
+          <div className="">LIFESTYLE</div>
+        </div>
+        {login ? (
+        <button className="bg-black px-2 py-1 rounded text-white max-h-8">MY PROFILE</button>
+        ) : (
+        <button className="bg-black px-2 py-1 rounded text-white max-h-8" onClick={()=>{setSignup(true); setBurger(false); a()}}>SIGN UP</button>
+      )}
+      </Modal>}
+
+
       <header className="min-h-screen">
         <nav className="flex justify-between p-2">
           <div className=""><img src={Logo} alt="" /></div>
@@ -54,10 +90,10 @@ const {products,addProduct} = useProducts()
         <button className="bg-black px-2 py-1 rounded text-white max-h-8" onClick={()=>setSignup(true)}>SIGN UP</button>
       )}
           </div>
-          <div className="display mt-5">
-            <div className="w-6 h-1 bg-gray-700 rounded mb-1 "></div>
-            <div className="w-6 h-1 bg-gray-700 rounded mb-1 second"></div>
-            <div className="w-6 h-1 bg-gray-700 rounded mb-1 third"></div>
+          <div className="display mt-5 z-40" onClick={()=>{a();setSignup(false)}}>
+            <div className="w-6 h-1 bg-gray-700 rounded mb-1 one"></div>
+            <div className="w-6 h-1 bg-gray-700 rounded mb-1 two"></div>
+            <div className="w-6 h-1 bg-gray-700 rounded mb-1 three"></div>
           </div>
 
         </nav>
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,7 +3,7 @@ export function Modal({title,children,setSignup}:any) {
     return(
         <>
         <div className="fixed bg-black/50 top-0 right-0 left-0 bottom-0 z-40"onClick={()=>{setSignup(false)}}></div>
-            <div className="mx-auto w-[500px] p-5 rounded bg-white fixed top-10 left-1/2 -translate-x-1/2 z-40">
+            <div className="mx-auto w-full p-5 rounded bg-white fixed left-1/2 -translate-x-1/2 z-40">
             <h1 className="text-center font-bold text-3xl">{title}</h1>
             {children}
             </div>
